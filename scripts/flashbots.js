7516d092456d8d5d987b17120aabf54051e4c506
Morten Bergseng <bergseng.m@gmail.com>
2022-05-30 15:26:35 +0200

    more

---
+++ scripts/flashbots.js
@@ -0,0 +1,5 @@
+const { FlashbotsBundleProvider, } = require("@flashbots/ethers-provider-bundle");
+const { BigNumber } = require("ethers");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
